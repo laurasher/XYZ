51b898b96e1e9bffdcbe100342d80300b0499003
laurasher <asherlk1@mac.com>
2016-02-18 10:06:49 -0800

    Save data as floats, plotting vector

--- XYZTest/src/XYZTestApp.cpp
+++ XYZTest/src/XYZTestApp.cpp
@@ -15,13 +15,22 @@ using namespace std;
 using asio::ip::udp;
 using namespace soso;
 
-std::vector<string> xyz(3); //should probably convert to float
+//std::vector<string> xyz(3); //should probably convert to float
+std::vector<float> xyz(3); //should probably convert to float
+
+float dx = 0.0f;
+float vx = 0.0f;
+float dy = 0.0f;
+float vy = 0.0f;
+float dz = 0.0f;
+float vz = 0.0f;
 
 class XYZTestApp : public App {
 public:
     void setup() override;
     void update() override;
     void draw() override;
+    void prepareSettings(Settings *settings);
 
 private:
     ci::Font mFont;
@@ -32,6 +41,11 @@ private:
     bool mFullScreen;
     std::shared_ptr<UdpServer> _udpServer;
 };
+
+void XYZTestApp::prepareSettings(Settings *settings)
+{
+    settings->setWindowSize(800, 600);
+}
 
 void XYZTestApp::setup()
 {
@@ -45,9 +59,13 @@ void XYZTestApp::setup()
         //Populate xyz vector
         std::vector<std::string> substrings = split(buff, ",");
         for (int i = 0; i < 3; i++) {
-            xyz[i] = substrings.back();
+            cout << substrings.back();
+            xyz[i] = std::stof(substrings.back());
             substrings.pop_back();
         }
+
+        //Turn xyz accel into position data
+        //        for (int i = 1; i < n; i++)
     });
 }
 
@@ -62,6 +80,14 @@ void XYZTestApp::update()
 void XYZTestApp::draw()
 {
     gl::clear(Colorf::black());
+    //Put measurements on screen
+    //Simple accel vector plot
+    gl::color(Color(1.0f, 0.0f, 0.0f));
+    float vectorLength = 15.0f;
+    float headLength = 6.0f;
+    float headRadius = 6.0f;
+
+    gl::drawVector(vec3(0, 600, 0), vec3(0, 0, 0), headLength, headRadius);
 }
 
 CINDER_APP(XYZTestApp, RendererGl)
